ba562702ccc5dc1c77b5aa14e9d1a21e429f0956
Mark Edward Ratio <markedwardratio@gmail.com>
2020-09-25 14:47:03 +0800

    Process the JSON data to return an object with only needed fields

--- src/App.js
+++ src/App.js
@@ -4,20 +4,42 @@ import './App.css';
 class App extends React.Component {
 	constructor() {
 		super();
-		this.state = {};
+		this.state = {
+			cityWeatherData: {},
+		};
 		this.getCityWeatherData = this.getCityWeatherData.bind(this);
+		this.processCityWeatherData = this.processCityWeatherData.bind(this);
+		this.setWeatherData = this.setWeatherData.bind(this);
+	}
+
+	componentDidMount() {
+		this.setWeatherData('quezon city');
 	}
 
 	async getCityWeatherData(cityName) {
 		const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 		const response = await fetch(url, { mode: 'cors' });
 		const cityWeatherData = await response.json();
-		console.log(cityWeatherData);
+		return cityWeatherData;
 	}
 
-	componentDidMount() {
-		this.getCityWeatherData('london');
+	processCityWeatherData(weatherData) {
+		return {
+			name: weatherData.name,
+			country: weatherData.sys.country,
+			temp: weatherData.main.temp,
+			description: weatherData.weather[0].description,
+		};
+	}
+
+	async setWeatherData(cityName) {
+		const cityWeatherData = await this.getCityWeatherData(cityName);
+		const processedCityWeatherData = this.processCityWeatherData(
+			cityWeatherData
+		);
+		this.setState({ cityWeatherData: processedCityWeatherData });
+		console.log(this.state.cityWeatherData);
 	}
 
 	render() {
