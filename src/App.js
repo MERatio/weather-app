ba23c1aeb9d03d55437a759524874c001d86c0bb
Mark Edward Ratio <markedwardratio@gmail.com>
2020-09-25 13:17:27 +0800

    Add a function that gets weather data of a city

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-	return <h1>Weather App</h1>;
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+		this.getCityWeatherData = this.getCityWeatherData.bind(this);
+	}
+
+	async getCityWeatherData(cityName) {
+		const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+		const response = await fetch(url, { mode: 'cors' });
+		const cityWeatherData = await response.json();
+		console.log(cityWeatherData);
+	}
+
+	componentDidMount() {
+		this.getCityWeatherData('london');
+	}
+
+	render() {
+		return <h1>Weather App</h1>;
+	}
 }
 
 export default App;
