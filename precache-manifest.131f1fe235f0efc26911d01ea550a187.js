5e33ad7bb0d87e0c6ae7b0f2ee9f858979934777
Mark Edward Ratio <markedwardratio@gmail.com>
2020-09-27 11:49:06 +0800

    Updates

---
+++ precache-manifest.131f1fe235f0efc26911d01ea550a187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e5dd981e62cdb630cb9349e09b5e79",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "15a46bba86221516fcb1",
+    "url": "/weather-app/static/css/main.24315c76.chunk.css"
+  },
+  {
+    "revision": "7918386fb45fdebdf0f9",
+    "url": "/weather-app/static/js/2.428fde58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.428fde58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a46bba86221516fcb1",
+    "url": "/weather-app/static/js/main.5253b21a.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
