a7041fee59d9704609a29989d5a9a3e5451e1e01
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-11-18 12:52:21 +0800

    Remove the processing of weather data

--- src/index.js
+++ src/index.js
@@ -7,53 +7,18 @@ const WEATHER_API_API_KEY = "7d3db2c9e6624ad8aed41548231611";
 const searchForm = document.getElementById("searchForm");
 const queryInput = searchForm.querySelector(".queryInput");
 
-function getDateStandardHour(date) {
-  return new Intl.DateTimeFormat(undefined, {
-    hour12: true,
-    hour: "numeric",
-  }).format(date);
-}
-
-function getDayName(date) {
-  return new Intl.DateTimeFormat(undefined, { weekday: "long" }).format(date);
-}
-
-function processWeatherData(weatherForecastData) {
-  const localDate = new Date(weatherForecastData.location.localtime);
-  const currentHourForecast =
-    weatherForecastData.forecast.forecastday[0].hour[localDate.getHours()];
-  return {
-    location: {
-      name: weatherForecastData.location.name,
-      country: weatherForecastData.location.country,
-    },
-    currentHour: {
-      temp_c: currentHourForecast.temp_c,
-      temp_f: currentHourForecast.temp_f,
-      chance_of_rain: currentHourForecast.chance_of_rain,
-      humidity: currentHourForecast.humidity,
-      wind_kph: currentHourForecast.wind_kph,
-      wind_mph: currentHourForecast.wind_mph,
-      dayName: getDayName(localDate),
-      standardHour: getDateStandardHour(localDate),
-      conditionIcon: `https:${currentHourForecast.condition.icon}`,
-      conditionText: currentHourForecast.condition.text,
-    },
-  };
-}
-
-async function fetchWeatherForecastData(query) {
+// Forecast data that is availbale is 3 days for free plan, which is what I'm using.
+async function fetchWeatherForecastData(query, days = 3) {
   const res = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_API_KEY}&q=${query}`,
+    `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_API_KEY}&q=${query}&days=${days}`,
   );
   const weatherForecastData = await res.json();
   return weatherForecastData;
 }
 
 async function logWeatherData(query) {
-  const weatherForecastData = await fetchWeatherForecastData(query);
-  const weatherData = processWeatherData(weatherForecastData);
-  console.log(weatherData);
+  const weatherForecastData = await fetchWeatherForecastData(query, 3);
+  console.log(weatherForecastData);
 }
 
 function handleSearchFormSubmit(e) {
